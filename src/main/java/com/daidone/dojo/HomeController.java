50914a9d3d1e29d4ff05ceeee3083b9987ebb04e
Frank Daidone <daidonef@msu.edu>
2016-11-22 10:29:58 -0500

    first commit

---
+++ src/main/java/com/daidone/dojo/HomeController.java
@@ -0,0 +1,38 @@
+package com.daidone.dojo;
+
+import java.text.DateFormat;
+import java.util.Date;
+import java.util.Locale;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+/**
+ * Handles requests for the application home page.
+ */
+@Controller
+public class HomeController {
+	
+	private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
+	
+	/**
+	 * Simply selects the home view to render by returning its name.
+	 */
+	@RequestMapping(value = "/enroll", method = RequestMethod.POST)
+	public String home(Model model, HttpServletRequest request) {
+		
+		model.addAttribute("firstname", request.getParameter("firstname"));
+		model.addAttribute("lastname", request.getParameter("lastname"));
+		model.addAttribute("email", request.getParameter("email"));
+		model.addAttribute("dojo", request.getParameter("dojo"));
+		
+		return "home";
+	}
+	
+}
---
+++ src/main/webapp/WEB-INF/views/home.jsp
@@ -0,0 +1,17 @@
+<%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c" %>
+<%@ page session="false" %>
+<html>
+<head>
+	<title>Home</title>
+</head>
+<body>
+<h1>
+	This Your Information and Class 
+</h1>
+
+<p>Your first name: ${firstname}. </p>
+<p>Your last name: ${lastname}</p>
+<p>Your email: ${email}</p>
+<p>Your Class: ${dojo}</p>
+</body>
+</html>
