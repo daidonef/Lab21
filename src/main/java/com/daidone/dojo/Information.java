e80acb440caad54e32c1923e04f5555d2f666a75
Frank Daidone <daidonef@msu.edu>
2016-11-25 09:32:18 -0500

    fiting Information Class

---
+++ src/main/java/com/daidone/dojo/Information.java
@@ -0,0 +1,17 @@
+package com.daidone.dojo;
+
+public class Information {
+	
+	public static String gettingURL () {
+ 		return "jdbc:mysql://127.0.0.1:3306/dojo";
+ 	}
+ 	
+ 	public static String gettingUsername () {
+ 		return "root";
+ 	}
+ 	
+ 	public static String gettingPassword () {
+ 		return "Frank452389";
+ 	}
+
+}
